d1eb792da0ac3926cee5336a31bb72cb128c926b
Spencer Bohol <zxtm89j1@gmail.com>
2022-08-25 17:17:22 +0800

    cont strings

--- script.js
+++ script.js
@@ -117,6 +117,28 @@ console.log(airline.indexOf('Portugal'));
 
 console.log(airline.slice(4));
 console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat! ');
+  } else {
+    console.log('You got lucky!');
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
 // restaurant.numGuests = 0;
 // const guests = restaurant.numGuests || 10;
 // console.log(guests);
